35b90ee54b8870230c781318b0b0bbc46019c1b1
Nathan <Naththo@outlook.com>
2015-07-23 17:42:30 +0100

    Fixed older button displaying even if there are no more logs to display

--- logs.php
+++ logs.php
@@ -12,7 +12,7 @@ if (!isset($current))
 	$current = 0;
 }
 
-$zncweb->tpl->assign("olderpage", ($zncweb->site->getTotal() < $CONFIG['site']['total_per_page'] ? false : $current + 1));
+$zncweb->tpl->assign("olderpage", (($zncweb->site->getTotal() < $CONFIG['site']['total_per_page'] || $zncweb->site->getTotal() / $CONFIG['site']['total_per_page'] > $current) ? false : $current + 1));
 $zncweb->tpl->assign("newerpage", ($current == 0 ? false : $current -1));
 $zncweb->tpl->assign("page_title", "Lol");
 $zncweb->tpl->assign("data", $zncweb->site->getRows((!isset($current) || $current == 0 ? false : ($current * $CONFIG['site']['total_per_page']))));
