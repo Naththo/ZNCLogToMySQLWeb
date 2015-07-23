27881a320ac35e1b3e8a63f2075796e65f4f7d5b
Nathan <Naththo@outlook.com>
2015-07-23 18:00:25 +0100

    Fix older page button

--- logs.php
+++ logs.php
@@ -1,7 +1,7 @@
 <?PHP
 
 require_once("common.php");
-
+define("PAGE_TITLE", "hi");
 if (isset($_GET['page']))
 {
 	$current = $_GET['page'];
@@ -12,9 +12,9 @@ if (!isset($current))
 	$current = 0;
 }
 
-$zncweb->tpl->assign("olderpage", (($zncweb->site->getTotal() < $CONFIG['site']['total_per_page'] || $zncweb->site->getTotal() / $CONFIG['site']['total_per_page'] > $current) ? false : $current + 1));
+$zncweb->tpl->assign("olderpage", (($zncweb->site->getTotal() < $CONFIG['site']['total_per_page'] || $zncweb->site->getTotal() / $CONFIG['site']['total_per_page'] < ($current+1)) ? false : $current + 1));
 $zncweb->tpl->assign("newerpage", ($current == 0 ? false : $current -1));
-$zncweb->tpl->assign("page_title", "Lol");
+$zncweb->tpl->assign("page_title", ($current > 0) ? "Logs | " . $current : "Logs");
 $zncweb->tpl->assign("data", $zncweb->site->getRows((!isset($current) || $current == 0 ? false : ($current * $CONFIG['site']['total_per_page']))));
 $zncweb->tpl->display("logs.tpl");
 
