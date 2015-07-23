e5337753eff91fe4d79f13adb66679d0dd647a68
Nathan <Naththo@outlook.com>
2015-07-23 18:54:34 +0100

    Add the lock check before loading files

--- install/common.php
+++ install/common.php
@@ -2,6 +2,12 @@
 define("IN_INSTALL", true);
 session_start();
 
+if (file_exists("lock"))
+{
+	$tpl->display("install/complete.tpl");
+	die();
+}
+
 require_once("../classes/3rdparty/smarty/libs/Smarty.class.php");
 
 $tpl = new Smarty();
@@ -9,12 +15,6 @@ $tpl->setTemplateDir("../tpl/templates/");
 $tpl->setCompileDir("../tpl/templates_compiled");
 $tpl->setCacheDir("../tpl/templates_cached");
 $tpl->assign("install", true);
-
-if (file_exists("lock"))
-{
-	$tpl->display("install/complete.tpl");
-	die();
-}
 
 require_once("../classes/Database.class.php");
 require_once("classes/Install.class.php");
